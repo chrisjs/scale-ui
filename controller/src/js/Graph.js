538887d89c8f152b4d321d85f69cd732dd4b0a71
Tim Crowley <tim@crowleyworks.com>
2018-06-07 21:14:43 -0400

    Added a fixed y-axis scale

--- controller/src/js/Graph.js
+++ controller/src/js/Graph.js
@@ -17,19 +17,20 @@ class Graph extends React.Component {
     render() {
         let scale_settings = this.props.scale_settings;
         if (scale_settings.length < 1) {
-            scale_settings = [100, 100, 100, 100, 100];
+            scale_settings = [1, 1, 1, 1, 1];
         }
 
         const BarSeries = VerticalBarSeriesCanvas; // : VerticalBarSeries;
         const graphData = [
-            { x: 2, y: scale_settings[1] / 100 },
-            { x: 4, y: scale_settings[2] / 100 },
-            { x: 6, y: scale_settings[3] / 100 },
-            { x: 8, y: scale_settings[4] / 100 }
+            { x: 2, y: scale_settings[1] },
+            { x: 4, y: scale_settings[2] },
+            { x: 6, y: scale_settings[3] },
+            { x: 8, y: scale_settings[4] }
         ];
         return (
             <div>
                 <XYPlot
+                    yDomain={[0, 200]}
                     width={500}
                     height={400}
                     stackBy="y">
